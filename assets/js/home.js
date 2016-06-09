c2c1d767bd5e318de53d643b4f3bfe5f1c0e2fe5
taniacoder <tania.cuyamedina@gmail.com>
2016-06-09 02:44:49 -0500

    lare

---
+++ assets/js/home.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$('.slider-col').slick({
+	  autoplay: true,
+  	  autoplaySpeed: 2000,	
+	  dots: true,
+	  infinite: false,
+	  speed: 200,
+	  slidesToShow: 6,
+	  slidesToScroll: 6,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	  ]
+	});    
+});
